eb0b920eb1966c242ce14f8a2a38074784269ba9
Tikiiiiiii <1967301917@qq.com>
2024-06-23 17:20:25 +0800

    vue个人主页学习项目初始化提交

---
+++ myvue/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="border">
+    测试
+  </div>
+</template>
+
+<style scoped>
+
+
+</style>
